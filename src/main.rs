bbcebc2c6ebb8e1a22ae347cc38655a159c7a18f
js k <js@github.com>
2023-07-24 23:27:38 +0900

    [update] endian_codec

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
-use bincode::{deserialize, serialize, Result};
-use serde::{Deserialize, Serialize};
+use endian_codec::{PackedSize, EncodeLE, DecodeLE, EncodeBE, DecodeBE};
+// If you look at this structure without checking the documentation, you know it works with
+// little-endian notation
+#[derive(Debug, PartialEq, Eq, PackedSize, EncodeLE, DecodeLE)]
+struct Version {
+  major: u32,
+  minor: u32,
+  patch: u32
+}
 
-use img_caster::bitarray;
+#[derive(Debug, PartialEq, Eq, PackedSize, EncodeBE, DecodeBE)]
+struct Version_be {
+  major: u32,
+  minor: u32,
+  patch: u32
+}
+
 
 fn main() {
-    let mut bitarray = bitarray::BitArray::new(55);
-    println! {"Bitarray {:?}", bitarray}
+    let mut buf = [0; Version::PACKED_LEN]; // From PackedSize
+    let test = Version { major: 0, minor: 21, patch: 37 };
+    println!("org: {:?}", test);
+    
+    // if you work with big- and little-endians, you will not mix them accidentally
+    test.encode_as_le_bytes(&mut buf);
+    println!("le: {:?}", buf);
 
-    bitarray.set(3, true);
-    println! {"Bitarray {:?}", bitarray}
+    let test_from_b = Version::decode_from_le_bytes(&buf);
+    println!("test_from_b: {:?}", test_from_b);
+    
+    let mut buf = [0; Version_be::PACKED_LEN]; // From PackedSize
+    let test = Version_be { major: 0, minor: 21, patch: 37 };
+    println!("org: {:?}", test);
+    
+    // if you work with big- and little-endians, you will not mix them accidentally
+    test.encode_as_be_bytes(&mut buf);
+    println!("le: {:?}", buf);
 
-    bitarray.set(5, true);
-    println! {"Bitarray {:?}", bitarray}
-
-    bitarray.set(20, true);
-    println! {"Bitarray {:?}", bitarray}
-
-    bitarray.set(53, true);
-    println! {"Bitarray {:?}", bitarray}
-
-    let packed = serialize(&bitarray).unwrap();
-    println! {"packed {:?}", packed}
-
-    let mut other = bitarray::BitArray::new(55);
-    other.set(1, true);
-    other.set(25, true);
-    other.set(48, true);
-
-    bitarray.bits_or(other);
-    let packed = serialize(&bitarray).unwrap();
-    println! {"packed {:?}", packed}
-    println! {"bits {:?}", bitarray.bits()}
-
+    let test_from_b = Version_be::decode_from_be_bytes(&buf);
+    println!("test_from_b: {:?}", test_from_b);
 }
