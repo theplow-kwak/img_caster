5710ee09373a34b058da8e70598136f8130a3c9f
js k <js@github.com>
2023-07-26 21:29:54 +0900

    update: buffer split_at_mut

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 
-
 #[derive(Debug)]
 struct Slice {
     slice_no: u32,
@@ -44,6 +43,10 @@ impl Man {
     }
 }
 
+fn test(buff: &mut [u8]) -> Result<&mut [u8], &str> {
+    let data = &mut buff[3..25];
+    Ok(data)
+}
 
 fn main() {
     let mut man = Man::new();
@@ -55,7 +58,27 @@ fn main() {
     println!("{:?}", slice1);
     slice1.end = 5;
     println!("{:?}", slice1);
-    
+
     let mut slice2 = man.get_slice(2);
     println!("{:?}", slice2);
+
+    let mut buf = [
+        1u8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 4, 8, 9, 10, 11, 12, 13, 14, 15,
+        16, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+    ]; // From PackedSize
+    {
+        println!("buf {:?}", &buf);
+        if let Ok(data) = test(&mut buf) {
+            println!("data {:?}", &data);
+        }
+        println!("buf {:?}", &buf);
+    }
+    let mut vbuf = buf.to_vec();
+    println!("b vec buf {:?}", &vbuf);
+    let (left, right) = vbuf.split_at_mut(19);
+    println!("a vec buf {:?}", &left);
+    println!("remain buf {:?}", &right);
+    vbuf = right.to_vec();
+    println!("b vec buf {:?}", &vbuf);
+    
 }
