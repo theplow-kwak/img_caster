8a8eec72ad69fab33225904133c60393aef975f0
js.kwak <theplow@naver.com>
2023-12-26 22:46:24 +0900

    fua

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,36 @@
-extern crate winapi;
+use clap::Parser;
 
-use winapi::shared::devguid::GUID_DEVCLASS_DISKDRIVE;
-use winapi::shared::ntdef::NULL;
-use winapi::um::cfgmgr32::{
-    CM_Get_Device_IDA, CM_Get_Parent, CM_Locate_DevNodeA, CM_Locate_DevNode_ExA, CR_SUCCESS,
-};
-use winapi::um::setupapi::{DIGCF_ALLCLASSES, HDEVINFO, SPDRP_DEVICEDESC};
+use img_caster::disk::Disk;
+
+#[derive(Parser, Default, Debug)]
+#[clap(author, version, about)]
+/// Receiver for Multicast File Transfer
+struct Args {
+    /// File name to save the received data.
+    #[clap(short, long, value_name = "FILE")]
+    filepath: Option<String>,
+
+    /// PhysicalDrive number. ex) 1 -> "\\.\PhysicalDrive1"
+    #[clap(short, long)]
+    driveno: Option<u8>,
+}
 
 fn main() {
-    unsafe {
-        let mut device_info_set: HDEVINFO = winapi::um::setupapi::SetupDiGetClassDevsA(
-            &GUID_DEVCLASS_DISKDRIVE,
-            NULL as *const i8,
-            NULL as *mut winapi::shared::windef::HWND__,
-            DIGCF_ALLCLASSES,
-        );
+    let args = Args::parse();
 
-        if device_info_set.is_null() {
-            println!("SetupDiGetClassDevs failed");
-            return;
-        }
-
-        let mut dev_info_data: winapi::um::setupapi::SP_DEVINFO_DATA = std::mem::zeroed();
-        dev_info_data.cbSize = std::mem::size_of::<winapi::um::setupapi::SP_DEVINFO_DATA>() as u32;
-
-        let mut index: u32 = 0;
-        let mut device_id: [u8; 1000] = [0; 1000];
-
-        while winapi::um::setupapi::SetupDiEnumDeviceInfo(
-            device_info_set,
-            index,
-            &mut dev_info_data,
-        ) != 0
-        {
-            if winapi::um::setupapi::SetupDiGetDeviceInstanceIdA(
-                device_info_set,
-                &mut dev_info_data,
-                device_id.as_mut_ptr() as *mut i8,
-                1000,
-                NULL as *mut u32,
-            ) != 0
-            {
-                let device_id_str =
-                    std::ffi::CStr::from_ptr(device_id.as_ptr() as *const i8).to_string_lossy();
-                println!("Device ID: {}", device_id_str);
-
-                let mut parent: winapi::um::cfgmgr32::DEVINST = std::mem::zeroed();
-                if winapi::um::cfgmgr32::CM_Get_Parent(&mut parent, dev_info_data.DevInst, 0)
-                    == CR_SUCCESS
-                {
-                    let mut device_id_parent: [u8; 1000] = [0; 1000];
-                    if winapi::um::cfgmgr32::CM_Get_Device_IDA(
-                        parent,
-                        device_id_parent.as_mut_ptr() as *mut i8,
-                        1000,
-                        0,
-                    ) == CR_SUCCESS
-                    {
-                        let device_id_parent_str =
-                            std::ffi::CStr::from_ptr(device_id_parent.as_ptr() as *const i8)
-                                .to_string_lossy();
-                        println!("Parent Device ID: {}", device_id_parent_str);
-                    }
-                }
-            }
-
-            index += 1;
-        }
-
-        winapi::um::setupapi::SetupDiDestroyDeviceInfoList(device_info_set);
+    let mut filename = String::from("");
+    if let Some(filepath) = args.filepath.as_deref() {
+        filename = filepath.to_string();
     }
+    if let Some(driveno) = args.driveno {
+        filename = format!("\\\\.\\PhysicalDrive{driveno}");
+    }
+
+    // Open file
+    let mut disk = Disk::open(filename.to_string(), 'w');
+    if let Some(ref d) = disk {
+        println!("{:?}", d);
+    }
+
+
 }
